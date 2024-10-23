5067c27e1f533a92f00f0e3fd9c30903669f28de
Infowithbharath <bharathkumar220502@gmail.com>
2024-10-23 17:37:59 +0530

    second commit

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -1,8 +1,8 @@
 // src/api.js
 import axios from "axios";
-
+const apiUrl = process.env.REACT_APP_BASE_URL;
 const api = axios.create({
-	baseURL: "http://localhost:3000/api", // Update to match the Express server port
+	baseURL: `${apiUrl}/api`, // Update to match the Express server port
 });
 
 export default api;
